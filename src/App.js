f109e1f1b4d0d5717c3b932c32e42ec16aebcd3b
Md Shayon <mdshayon0@gmail.com>
2019-03-31 03:00:06 +0600

    Initial code setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Main file
+      </div>
+    )
+  }
+}
+
+export default App;
+
