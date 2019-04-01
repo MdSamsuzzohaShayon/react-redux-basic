eea52e1f298cde4f8c2807352a2df7f6c516f633
Md Shayon <mdshayon0@gmail.com>
2019-04-01 18:24:32 +0600

    NavLink and Link to Prevent default action

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
 
+// IT DIDN'T REFRESH THE PAGE OF REQUEST TO SERVER  
+import {Link, NavLink} from 'react-router-dom'; // this prevent the default action of request to server
+// NAVLINK WORK AS SAME WITH LINK BUT IT ADD AN EXTRA CLASS OF ACTIVE
+//  INSPECT ELEMENT TO SEE THAT
+
+
+
 const Navbar = () => {
     return (
         <Menu inverted color="green">
-            <Menu.Item href="/" name='home' content='Home' ></Menu.Item>
-            <Menu.Item href="/about" name='about' content='About' ></Menu.Item>
-            <Menu.Item href="/contract" name='contract' content='Contract' ></Menu.Item>
+            <Menu.Item name='home' ><Link to='/'>Home</Link></Menu.Item>
+            <Menu.Item name='about' ><NavLink to='/about'>About</NavLink></Menu.Item>
+            <Menu.Item name='contract'><NavLink to='/contract'>Contract</NavLink></Menu.Item>
             <br/><br/>
         </Menu>
     )
