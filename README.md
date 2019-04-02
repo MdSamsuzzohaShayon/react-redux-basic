17be791962d587a662caf08f7b30a063094c6988
Md Shayon <mdshayon0@gmail.com>
2019-04-02 16:51:34 +0600

    Codepen basic redux project

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 
 install react router
 ```npm install react-router-dom```
-and import
+
+
 
 [Higher Order Component](https://reactjs.org/docs/higher-order-components.html)
 
@@ -16,6 +17,16 @@ install  axios ```npm install axios```
 
 # Redux
 
+```npm install redux react-redux```
+
+[Basic Concept in Codepen](https://codepen.io/mdsamsuzzohashayon/pen/EJjZwx)
+
+[Official docs](https://redux.js.org/introduction/getting-started)
+
 ![basic understanding](/img/Screenshot_1.png)
 
 ![WOrd flow](/img/Screenshot_3.png)
+ 
+ - A good place to create store is **index.js** because this is where we start our react app
+  - we need to create reducer to put into store
+  - Combine all reducer with store
--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'semantic-ui-css/semantic.min.css';
+import {createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// PROVIDER WILL SURROUND AND WRAP THE ROOT APP COMPONENT
+// WE WILL PASS STORE INTO PROVIDER SO STORE CAN INTRACT WITH REACT
+import {Provider} from 'react-redux'; 
+
+
+//CREATE STORE AND INVOKE FUNCTION
+const store = createStore();
+
+ReactDOM.render(<Provider store={store}><App /></Provider> , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
