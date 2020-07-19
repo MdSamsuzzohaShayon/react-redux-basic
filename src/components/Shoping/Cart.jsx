7ace460a90919254693ef4fb71727cfcd5322bf3
Md Shayon <mdshayon0@gmail.com>
2020-07-20 04:00:48 +0600

    complete project but the problem is by using coupon code we can get discount multiple times

--- src/components/Shoping/Cart.jsx
+++ src/components/Shoping/Cart.jsx
@@ -2,7 +2,15 @@ import React, { useState } from 'react';
 import './Cart.css';
 
 
-function menu() {
+
+const circle=(quantity)=>{
+    return (
+        <span className="circle">{quantity}</span>
+    )
+}
+
+// MAKING A SIMPLE MENU
+function Menu({quantity, addToCart}) {
     return (
         <div className="menu">
             <div className="items">
@@ -15,6 +23,7 @@ function menu() {
                 <ul className="item-2">
                     <li><a href="#">Cart
                     <img src="../icon/shopping-cart.svg" width="20px" alt="" />
+                    {addToCart == true? circle(quantity): null}
                     </a></li>
                     <li><a href="#"> <strong>En</strong> / বাংলা</a></li>
                 </ul>
@@ -23,10 +32,81 @@ function menu() {
     )
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 function Cart() {
+    // GENERAL PRICE
+    let defaultGeneralPrice = 13;
+    const [generalPrice, setGeneralPrice] = useState(defaultGeneralPrice);
+
+    // QUANTITY
+    const [quantity, setQuantaty] = useState(1);
+
+    // DISCOUNT
+    let dafaultDiscount = 5;
+    // let defaultCouponDiscount = quantity * dafaultDiscount ;
+    const [couponDiscount, setCouponDiscount] = useState(dafaultDiscount);
+    console.log(couponDiscount);
+
+    // TOTAL PRICE
+    // let defaultTotalPrice = generalPrice * quantity - couponDiscount ;
+    // console.log(defaultTotalPrice);
+    const [totalPrice, setTotalPrice] = useState(generalPrice);
+
+    const [addToCart, setAddToCart] = useState(false);
+    console.log(`Add to cart button click ? ${addToCart}`);
+
+
+
+
+    function incrementQuentaty(){
+        // EVERYTIME WHEN CLICK PLUS BUTTON IT WILL INCREMENT BY ONE
+        // AND TOTAL PRICE WILL BE CHANGE
+        // PRICE * QUANITY = TOTAL 
+        setQuantaty(prevQuantity=> prevQuantity+1);
+        setTotalPrice(prevPrice=> prevPrice + generalPrice);
+    }
+    function decrementQuantity(){
+        setQuantaty(prevQuantity=> prevQuantity-1);
+        setTotalPrice(prevPrice=> prevPrice - generalPrice);
+    }
+    function usingCouponDiscount(e){
+        e.preventDefault();
+        console.log(e.target.value);
+        if(e.target.value == 123){
+            console.log('set total price and coupon discount');
+            setCouponDiscount(prevDiscount => prevDiscount);
+            setTotalPrice(prevPrice=> prevPrice - couponDiscount * quantity);
+        }
+    }
+
+
+
+
+    function  handleClick(e){
+        e.preventDefault();
+        setAddToCart(prevAddToCart => prevAddToCart=true);
+    }
+
+
+
+
+
+
     return (
         <div className="Cart">
-            {menu()}
+            <Menu quantity={quantity} addToCart={addToCart} />
             <br />
             <br />
             <br />
@@ -40,24 +120,24 @@ function Cart() {
                         FC Bayern Munich First team squad First team squad Women's team The FC Bayern Women represent the club in the Bundesliga and Women's Champions League.
                     </div>
                 </div>
-                <div className="set-quan">
-                    <div className="quantaty">Quantaty 1</div>
+                <div className="set-quan set-bg">
+                    <div className="quantaty">Quantaty {quantity}</div>
                     <div className="btns">
-                        <button className="btn">+</button>
-                        <button className="btn">-</button>
+                        <button className="btn" onClick={incrementQuentaty}>+</button>
+                        <button className="btn" onClick={decrementQuantity}>-</button>
                     </div>
                 </div>
                 <div className="price">
-                    <div className="gp">General price 13$</div>
-                    <div className="coupon">Use Coupon 
+                    <div className="gp">General price {generalPrice}$</div>
+                    <div className="coupon set-bg">Use Coupon 
                         <form >
-                            <input placeholder="Enter your coupon code" type="text"/>
+                            <input onChange={usingCouponDiscount} placeholder="Enter your coupon code" type="text"/>
                         </form>
                     </div>
-                    <div className="total-price">Total Price 13$</div>
+                    <div className="total-price">Total Price {totalPrice}$</div>
                 </div>
-                <div className="add">
-                    <button className="btn">Add to Cart</button>
+                <div className="add set-bg">
+                    <button className="btn" onClick={handleClick} >Add to Cart</button>
                 </div>
             </div>
         </div>
